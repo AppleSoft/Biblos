ad80600344227b6b09c7caa08e8311bf1c2ff118
mircosx <mircosx@gmail.com>
2011-08-30 13:03:27 +0200

    v0.0.4 Limpieza carpetas, integracion sistema de filtrado estilo Google

--- auto/blabla.php
+++
@@ -1,4 +0,0 @@
-<textarea cols="20" rows="9" name="texto" id="formulario_comentar_comentarios_texto" 
-onfocus="if(this.value=='Escribe algo') value='';" 
-onblur="if(this.value=='') value='Escribe algo';"
->Escribe algo</textarea>
--- menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "<h1>Bienvenido a la biblioteca</h1>";
-
-?>
